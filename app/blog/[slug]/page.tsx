b5a19e88642992c12097de84a6e28d384c4c89cc
Nguyen Truong Quang <nguyentruongquang13@gmail.com>
2026-07-24 14:55:26 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -13,6 +13,7 @@ export default function Page({ params }: Params) {
     <>
       <h1>Slug: {params.slug}</h1>
       <p>Nguyen Truong Quang</p>
+      <p>Welcome to my project</p>
     </>
   );
 }
