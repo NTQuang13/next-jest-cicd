387a0fed031f1af9f679756b8324bbf273353351
Nguyen Truong Quang <nguyentruongquang13@gmail.com>
2026-07-24 15:25:00 +0700

    update

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -5,12 +5,26 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Page from "./page";
 
-it("App Router: Works with dynamic route segments", () => {
-  render(<Page params={{ slug: "Test" }} />);
+it("App Router: Works with dynamic route segments", async () => {
+  const page = await Page({
+    params: Promise.resolve({
+      slug: "Test",
+    }),
+  });
+
+  render(page);
+
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
 });
 
-it("Nguyen Truong Quang in the document", () => {
-  render(<Page params={{ slug: "Test" }} />);
+it("Nguyen Truong Quang in the document", async () => {
+  const page = await Page({
+    params: Promise.resolve({
+      slug: "Test",
+    }),
+  });
+
+  render(page);
+
   expect(screen.getByText("Nguyen Truong Quang")).toBeInTheDocument();
 });
